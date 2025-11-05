1fcd1d5715f3fa6dd22c24243d5efd6b36e5cf4b
richardkrausbusiness-cmd <richardkrausbusiness@gmail.com>
2025-11-05 21:41:50 +0100

    littleWork

--- src/crypto/crypto.py
+++ src/crypto/crypto.py
@@ -4,32 +4,32 @@ from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 import oqs
 
-class KeyGeneration:
+class KeyPair:
     def __init__(self):
         self.privX25519 = x25519.X25519PrivateKey.generate()
         self.pubX25519 = self.privX25519.public_key()
+        self.peerX25519
 
         self.algQP = "Kyber512"
         self.privKyber = oqs.KeyEncapsulation(self.algQP)
         self.pubKyber = self.privKyber.generate_keypair()
+        self.peerKyber
 
-    def get_public_keys(self):
+    def export_public(self):
+        exportX25519 = self.pubX25519.public_bytes()
+        exportKyber = self.pubKyber
+        return exportX25519, exportKyber
 
-        x25519_bytes = self.pubX25519.public_bytes(
-            encoding=serialization.Encoding.Raw,
-            format=serialization.PublicFormat.Raw
-        )
-        print("---------------------1")
+    def load_peer_keys(self, x25519Bytes, kyberBytes):
+        self.peerX25519 = x25519.X25519PublicKey.from_public_bytes(x25519Bytes)
+        self.peerKyber = kyberBytes
 
-        print("X25519 public key:")
-        print(x25519_bytes.hex())
+        return True
 
-        print("---------------------2")
-
-        print("\nPost-Quantum public key (Kyber512):")
-        print(self.pubKyber.hex())
-
-
-if __name__ == "__main__":
-    keys = KeyGeneration()
-    keys.get_public_keys()
+class HybridKeyExchange:
+    def __init__(self):
+        self.privX25519
+        self.peerX25519
+        
+        self.privKyber
+        self.peerKyber
